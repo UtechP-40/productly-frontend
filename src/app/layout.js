5ddc7cdce033c1e31b21bd34fe3e5620316cb49f
UtechP-40 <pradeep2420pradeep@gmail.com>
2025-07-19 03:10:14 +0530

    added auth Provider in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import "./
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../hooks/useAuth";
 
@@ -23,7 +25,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-gray-900`}
       >
-        {children}
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
