c30bdf1e6cfda9c67814daf46b15638237d7dd60
UtechP-40 <pradeep2420pradeep@gmail.com>
2025-07-19 03:12:06 +0530

    made a centralized provider for all all providers to not cluster the main code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { AuthProvider } from "../hooks/useAuth";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-gray-900`}
       >
-        <AuthProvider>
+        <Providers>
           {children}
-        </AuthProvider>
+        </Providers>
       </body>
     </html>
   );
